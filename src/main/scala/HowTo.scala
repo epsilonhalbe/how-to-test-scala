5c06fe52759772003787ac5ae752bcf74b2b7b7e
Martin Heuschober (Iron) <martin_heuschober@trimble.com>
2021-10-15 11:02:31 +0100

    contrived be thy example

--- src/main/scala/HowTo.scala
+++ src/main/scala/HowTo.scala
@@ -2,4 +2,9 @@ object HowTo extends App {
   def cube(x: Int) = {
     x * x * x
   }
+  def contrivedExample(x: List[Int]) : Either[String,List[Int]] = {
+    if (x.exists(elem => elem % 3 == 0 && elem > 1)) {
+      Left("Oh what a contrived example")
+    } else Right(x)
+  }
 }
